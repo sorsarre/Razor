9c60390039ee3567a97cd12b43471382e20eeabc
Andrey Kaydalov <sorsarre@gmail.com>
2021-09-20 00:41:30 +0300

    [ui-isolation] Make calls to AgentTabManager dispatch to existing impl if new impl is not available

---
+++ Razor/UI/Agents/AgentManagerFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assistant.Agents;
+
+namespace Assistant.UI.Agents
+{
+    class AgentManagerFactory
+    {
+        public IAgentManager CreateAgentManager(Agent agent, AgentControls controls)
+        {
+            switch (agent)
+            {
+                case UseOnceAgent a: return new UseOnceAgentManager(a, controls);
+                default: return null;
+            }
+        }
+    }
+}
--- Razor/UI/Agents/AgentTabManager.cs
+++ Razor/UI/Agents/AgentTabManager.cs
@@ -11,6 +11,8 @@ namespace Assistant.UI.Agents
     public class AgentTabManager
     {
         private static AgentControls _controls;
+        private static AgentManagerFactory _factory = new AgentManagerFactory();
+        private static IAgentManager _currentManager;
         private static ComboBox Agents => _controls.AgentList;
         private static GroupBox Group => _controls.Group;
         private static ListBox SubList => _controls.SubList;
@@ -50,6 +52,13 @@ namespace Assistant.UI.Agents
             }
         }
 
+        public static void OnButtonPress(int index)
+        {
+            if (_currentManager != null)
+                _currentManager.OnButtonPress(index);
+            else
+                CurrentAgent.OnButtonPress(index);
+        }
 
         private static Agent CurrentAgent => Agents.SelectedItem as Agent;
 
@@ -72,6 +81,12 @@ namespace Assistant.UI.Agents
                 Group.Visible = true;
                 Group.Text = CurrentAgent.Name;
                 SubList.Visible = true;
+                _currentManager?.Detach();
+                _currentManager = _factory.CreateAgentManager(CurrentAgent, _controls);
+                if (_currentManager != null)
+                    _currentManager?.OnSelected();
+                else
+                    CurrentAgent.OnSelected(SubList, Buttons);
             }
         }
     }
