b220136bba0a403714dc5c683c9bfcd20c8258f7
Andrey Kaydalov <sorsarre@gmail.com>
2021-09-20 01:37:36 +0300

    Clean up usings

--- Razor/UI/Agents/AgentManagerFactory.cs
+++ Razor/UI/Agents/AgentManagerFactory.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Assistant.Agents;
+﻿using Assistant.Agents;
 
 namespace Assistant.UI.Agents
 {
