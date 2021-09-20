477efa1478ae493b327f5bee162b574dba6aa395
Andrey Kaydalov <sorsarre@gmail.com>
2021-09-20 22:02:20 +0300

    [ui-isolation] Split UI from Restock agent

--- Razor/UI/Agents/AgentManagerFactory.cs
+++ Razor/UI/Agents/AgentManagerFactory.cs
@@ -12,6 +12,7 @@ namespace Assistant.UI.Agents
                 case SellAgent a: return new SellAgentManager(a, controls);
                 case SearchExemptionAgent a: return new SearchExemptionAgentManager(a, controls);
                 case ScavengerAgent a: return new ScavengerAgentManager(a, controls);
+                case RestockAgent a: return new RestockAgentManager(a, controls);
                 default: return null;
             }
         }
